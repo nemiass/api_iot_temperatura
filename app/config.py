90b186092b39c9dd3f5de5f2bd350278f0c7deaf
Jaime Ponce <nehemiastf@gmail.com>
2022-05-27 21:24:45 -0500

    sql alchemy echo to false

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 class DevCofig:
     # local
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@localhost:3306/iot_temperatura"
-    SQLALCHEMY_ECHO = True
+    SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     PROPAGATE_EXCEPTIONS = True
